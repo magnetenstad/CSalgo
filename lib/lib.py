34dde108ead348087275b6d73decad8313360f52
magnetenstad <tenstad.magne@gmail.com>
2020-12-19 21:38:44 +0100

    Update lib.py

--- lib/lib.py
+++ lib/lib.py
@@ -8,11 +8,10 @@ def compute(func, variables):
 		print(f"\nERROR - {e}")
 
 def ask(questions, types=None):
-	while True:
-		try:
-			if types == None:
-				return [input(f"{q}: ") for q in questions]
-			else:
-				return [types[i](input(f"<{types[i].__name__}> {q}: ")) for i, q in enumerate(questions)]
-		except Exception as e:
-			print(f"ERROR - {e}")
+	try:
+		if types == None:
+			return [input(f"{q}: ") for q in questions]
+		else:
+			return [types[i](input(f"<{types[i].__name__}> {q}: ")) for i, q in enumerate(questions)]
+	except Exception as e:
+		print(f"ERROR - {e}")
